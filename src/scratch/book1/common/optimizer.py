e72719e893feece1609b8c3a60cbc0db2fafd6e1
lee-seongdong <lee-seongdong@nts-corp.com>
2026-02-24 08:56:47 +0900

    Study dl

---
+++ src/scratch/book1/common/optimizer.py
@@ -0,0 +1,17 @@
+"""
+Optimizer for deep learning
+경사하강법을 이용한 최적화 클래스들을 모아둔 모듈
+"""
+
+class SGD:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
